9a80fadfd758350effdd03a4210b4e4c607d1de6
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-07-08 23:39:55 +0300

    complete

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function increment(i) {
+    let number = i;
+    number++;
+    return number;
+}
+
+function decrement(i) {
+    let number = i;
+    number--;
+    return number;
+}
+
+function makeInt(i) {
+    return parseInt(i, 10);
+}
+
+function preserveDecimal(i) {
+    return parseFloat(i);
+}
